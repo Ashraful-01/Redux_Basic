f0ed833bbc3c6323031071131ad7a2b166eca750
Ashraful Alam <mdmashraful1998@gmail.com>
2022-09-30 02:52:58 +0600

    dispatching store action

--- index.js
+++ index.js
@@ -8,9 +8,36 @@ const initState = {
 
 //create reducer
 const rootReducer = (state = initState, action) => {
+    if (action.type === 'INC_NUM') {
+        return {
+            ...state,
+            num: state.num + 1
+        }
+    }
+    if (action.type === 'ADD_NUM') {
+        return {
+            ...state,
+            num: state.num + action.value
+        }
+    }
     return state;
 }
 //store 
 
 const store = redux.createStore(rootReducer);
+console.log(store.getState());
+
+//dicpatching Action 
+
+// console.log(store);
+//when i define type it must be all upperCase 
+store.dispatch({
+    type: 'INC_NUM'
+})
+
+store.dispatch({
+    type: 'ADD_NUM',
+    value: 34
+})
+
 console.log(store.getState());
